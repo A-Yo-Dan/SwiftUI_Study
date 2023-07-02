d196a4e4e8f3defef281e23186bc07270bdaedcc
EunsuSeo01 <eunsu0613@naver.com>
2023-07-02 23:53:34 +0900

    [Week1] HelloWorld

---
+++ seori/week1/HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 서은수 on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
