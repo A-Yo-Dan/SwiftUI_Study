3f4970fc6a29829a98a7dc6e0ee55d25882bec26
EunsuSeo01 <eunsu0613@naver.com>
2023-07-02 23:54:13 +0900

    [Week1] AsyncImage 예제

---
+++ seori/week1/SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by 서은수 on 2023/07/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
