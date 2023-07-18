d018b2b0e705314084a69922ee5d6f1dd3cc349e
EunsuSeo01 <eunsu0613@naver.com>
2023-07-18 23:34:33 +0900

    [Week3] 섹션 16 Slot Machine 프로젝트 완료

---
+++ seori/week3/Slot-Machine/Slot-Machine/PlaySound.swift
@@ -0,0 +1,21 @@
+//
+//  PlaySound.swift
+//  Slot-Machine
+//
+//  Created by 서은수 on 2023/07/18.
+//
+
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("ERROR: Could not find and play the sound file!")
+        }
+    }
+}
