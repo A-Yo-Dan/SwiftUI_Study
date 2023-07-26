a3a3745c5d920e64270d39f4c8ffcd5b5d11e84e
youz2me <kynhun20@gachon.ac.kr>
2023-07-27 06:13:17 +0900

    week4 update #59

---
+++ zimging/week4/AFRICA/Students/Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,12 @@
+//
+//  CoverImageModel.swift
+//
+//  Created by YOUJIM on 2023/07/26.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+  let id: Int
+  let name: String
+}
---
+++ zimging/week4/AFRICA/Students/Africa/Africa/Model/VideoModel.swift
@@ -0,0 +1,18 @@
+//
+//  VideoModel.swift
+//
+//  Created by YOUJIM on 2023/07/26.
+//
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+  let id: String
+  let name: String
+  let headline: String
+  
+  // Computed Property
+  var thumbnail: String {
+    "video-\(id)"
+  }
+}
