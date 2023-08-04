f28b07ad631e9d995065018b3f207a248274829e
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2023-08-04 23:55:35 +0900

    #78 Section 16 실습파일

---
+++ ham/week6/Section 16. GAME SWIFTUI-APP-04/Students/Slot Machine/Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by 이재혁 on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
