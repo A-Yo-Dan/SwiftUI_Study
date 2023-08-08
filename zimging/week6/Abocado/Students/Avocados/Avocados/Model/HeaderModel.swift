46432999fe31ee5b26f44980e72530724d0e71b6
youz2me <kynhun20@gachon.ac.kr>
2023-08-09 02:06:51 +0900

    Week6 Update

---
+++ zimging/week6/Abocado/Students/Avocados/Avocados/Model/HeaderModel.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+// MARK: - HEADER MODEL
+
+struct Header: Identifiable {
+  var id = UUID()
+  var image: String
+  var headline: String
+  var subheadline: String
+}
---
+++ zimging/week6/Honeymoon/Students/Honeymoon/Honeymoon/View/HeaderView.swift
@@ -0,0 +1,66 @@
+//
+//  Created by Robert Petras
+//  SwiftUI Masterclass ♥ Better Apps. Less Code.
+//  https://swiftuimasterclass.com 
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+  // MARK: - PROPERTIES
+  @Binding var showGuideView: Bool
+  @Binding var showInfoView: Bool
+  let haptics = UINotificationFeedbackGenerator()
+  
+  var body: some View {
+    HStack {
+      Button(action: {
+        // ACTION
+        playSound(sound: "sound-click", type: "mp3")
+        self.haptics.notificationOccurred(.success)
+        self.showInfoView.toggle()
+      }) {
+        Image(systemName: "info.circle")
+          .font(.system(size: 24, weight: .regular))
+      }
+      .accentColor(Color.primary)
+      .sheet(isPresented: $showInfoView) {
+        InfoView()
+      }
+      
+      Spacer()
+      
+      Image("logo-honeymoon-pink")
+        .resizable()
+        .scaledToFit()
+        .frame(height: 28)
+      
+      Spacer()
+      
+      Button(action: {
+        // ACTION
+        playSound(sound: "sound-click", type: "mp3")
+        self.haptics.notificationOccurred(.success)
+        self.showGuideView.toggle()
+      }) {
+        Image(systemName: "questionmark.circle")
+          .font(.system(size: 24, weight: .regular))
+      }
+      .accentColor(Color.primary)
+      .sheet(isPresented: $showGuideView) {
+        GuideView()
+      }
+    }
+    .padding()
+  }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+  @State static var showGuide: Bool = false
+  @State static var showInfo: Bool = false
+  
+  static var previews: some View {
+    HeaderView(showGuideView: $showGuide, showInfoView: $showInfo)
+      .previewLayout(.fixed(width: 375, height: 80))
+  }
+}
